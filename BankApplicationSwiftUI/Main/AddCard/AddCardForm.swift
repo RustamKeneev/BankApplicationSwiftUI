c0d929f37e4c85bc2b70c1afcfc4379ab41987d5
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-04-21 20:58:05 +0600

    added card form

--- BankApplicationSwiftUI/Main/AddCard/AddCardForm.swift
+++ BankApplicationSwiftUI/Main/AddCard/AddCardForm.swift
@@ -11,12 +11,49 @@ struct AddCardForm: View {
     @Binding var shouldPresentAddCardForm: Bool
     
     @State private var name = ""
-
+    @State private var cardNumber = ""
+    @State private var limit = ""
+    @State private var cardType = "Visa"
+    @State private var month = 1
+    @State private var year = Calendar.current.component(.year, from: Date())
+    @State private var color = Color.blue
+    
+    let currentYear = Calendar.current.component(.year, from: Date())
+    
     var body: some View {
         NavigationView{
             Form{
-                Text("Add card form")
-                TextField("Name", text: $name)
+                Section(header: Text("Card information")) {
+                    TextField("Name", text: $name)
+                    TextField("Credit Card Number", text: $cardNumber)
+                        .keyboardType(.numberPad)
+                    TextField("Credit Limit", text: $limit)
+                        .keyboardType(.numberPad)
+                    Picker("Type", selection: $cardType){
+                        ForEach(["Visa", "MasterCard", "Discover"], id: \.self){ cardType in
+                            Text(String(cardType)).tag(String(cardType))
+                        }//: LOOP
+                    }//: PICKER
+                }//: SECTION CARD INFO
+                
+                Section(header: Text("Expiratioin")) {
+                    Picker("Month", selection: $month){
+                        ForEach(1..<13, id: \.self){ num in
+                            Text(String(num)).tag(String(num))
+                        }//: LOOP
+                    }//: PICKER MONTH
+                    
+                    Picker("Year", selection: $year){
+                        ForEach(currentYear..<currentYear + 20, id: \.self){ num in
+                            Text(String(num)).tag(String(num))
+                        }//: LOOP
+                    }//: PICKER YEAR
+                }//: SECTION EXPIRATION
+                
+                Section(header: Text("Color")) {
+                    ColorPicker("Color", selection: $color )
+                }//: SECTION COLOR
+                
             }//: FORM
             .navigationTitle("Add credit card")
             .navigationBarItems(leading: Button(action: {
