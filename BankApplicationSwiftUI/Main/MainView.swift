fc853b39860e9bf1077dfba4d7a6d6376b36622d
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-04-23 18:24:07 +0600

    Save Cards and FetchRequests with Core Data

--- BankApplicationSwiftUI/Main/MainView.swift
+++ BankApplicationSwiftUI/Main/MainView.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct MainView: View {
     
     @State var shouldPresentAddCardForm = false
     
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Card.timestamp, ascending: true)],
+        animation: .default)
+    private var cards: FetchedResults<Card>
+    
     var body: some View {
         NavigationView{
             ScrollView{
+                if !cards.isEmpty{
                 TabView{
-                    ForEach(0..<5){ num in
+                    ForEach(cards){ card in
                         CreditCardView()
                             .padding(.bottom, 50)
                     }//: LOOP
@@ -23,15 +32,54 @@ struct MainView: View {
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
                 .frame(height: 280)
                 .indexViewStyle(.page(backgroundDisplayMode: .always))
-                
+                }
                 Spacer()
                     .fullScreenCover(isPresented: $shouldPresentAddCardForm, onDismiss: nil){
                         AddCardForm(shouldPresentAddCardForm: $shouldPresentAddCardForm)
                     }
             }//: SCROLL
             .navigationTitle("Credit Cards")
-            .navigationBarItems(trailing: addCardButton)
+            .navigationBarItems(
+                leading: HStack {
+                addItemButton
+                deleteAllButton
+            },
+                trailing: addCardButton)
         }//: NAVIGATION VIEW
+    }
+    
+    private var deleteAllButton: some View {
+        Button{
+            cards.forEach { card in
+                viewContext.delete(card)
+            }
+            do {
+                try viewContext.save()
+            }catch{
+                
+            }
+        } label: {
+            Text("Delete all")
+        }
+    }
+    
+    var addItemButton: some View{
+        Button(action: {
+            withAnimation {
+//                offsets.map { items[$0] }.forEach(viewContext.delete)
+                let viewContext = PersistenceController.shared.container.viewContext
+                let card = Card(context: viewContext)
+                card.timestamp = Date()
+                do {
+                    try viewContext.save()
+                } catch {
+//                    let nsError = error as NSError
+//                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
+            }
+        }, label: {
+            Text("Add Item")
+        })
     }
     
     var addCardButton: some View {
